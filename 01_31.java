da45dfaf10caf2b36ef9ebd9745e54eff6089fb4
zhaoziheng-name <527492225@qq.com>
2020-01-31 18:26:06 +0800

    提交代码

---
+++ 01_31.java
@@ -0,0 +1,73 @@
+import com.sun.org.apache.xpath.internal.objects.XString;
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.List;
+public class test {
+    public static void main(String[] args) {
+//        int num = 10;
+//        System.out.println(Integer.MAX_VALUE);
+//        System.out.println(Integer.MIN_VALUE);
+//        int maxValue = Integer.MAX_VALUE;
+//        System.out.println(maxValue + 1);
+//        int minValue = Integer.MIN_VALUE;
+//        System.out.println(minValue - 1);
+//        long num = 10L;
+//        System.out.println(num);
+//        System.out.println(Long.MAX_VALUE);
+//        System.out.println(Long.MIN_VALUE);
+//        double a = 1;
+//        double b = 2;
+//        System.out.println(a / b);
+//        double num = 1.1;
+//        System.out.println(num * num);
+//        char ch = '呵';
+//        System.out.println(ch);
+//        int age = 2;
+//        String name = "66";
+//        Dog dog = new Dog(name, age);
+//        int x = age - 1;
+//        if (x > 1) {
+//            dog.play();
+//        } else {
+//            dog.bark();
+//        }
+//        int[] numList = new int[]{1, 3, 5, 7, 9};
+//        new Thread(() -> System.out.println("hello world")).start();
+//        try {
+//            InputStream in = new FileInputStream(new File("//xx.txt"));
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        List<Integer> List = Arrays.asList(1, 2, 2, 4, 5, 6, 7, 10);
+//        list.stream()
+//                .filter(e -> e % 2 == 0)
+//                .distinct()
+//        forEach(System.out::println);
+//    }
+//    }
+//    Class Dog {
+//    private String name;
+//    private Integer age;
+//    public Dog(String name, Integer age) {
+//        this.name = name;
+//        this.age = age;
+//        }
+//        public void play() {
+//        System.out.println(this.name + "want to play");
+//        }
+//        public void bark() {
+//        System.out.println(this.name + "汪汪汪");
+//        }
+//        String a = "hello";
+//        String b = "world";
+//        String c = a + b;
+//        System.out.println(c);
+        String str = "result = ";
+        int a = 10;
+        int b = 20;
+        String result = str + a + b;
+        System.out.println(result);
+    }
+}
