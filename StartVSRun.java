dee25bef877e7761c6c1cbad05f165ecdb9ad672
zhaoziheng-name <527492225@qq.com>
2020-05-16 18:25:03 +0800

    提交代码

---
+++ StartVSRun.java
@@ -0,0 +1,22 @@
+package java_0516;
+
+public class StartVSRun {
+    public static void main(String[] args) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("第一滴血");
+            }
+        },"子线程Thread Baby").start();
+        new Thread(() -> {
+            System.out.println("第一滴血");
+        },"子线程Thread Baby").start();
+        System.out.println("main");
+
+        for (int i = 0; i < 5; i++) {
+            new Thread(() -> {
+                System.out.println(Thread.currentThread().getName());
+            }).start();
+        }
+    }
+}
