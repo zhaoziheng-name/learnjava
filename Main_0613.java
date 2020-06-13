73c786e9f04734c2fd0cf2ab1a60f4d08cd04b3a
zhaoziheng-name <527492225@qq.com>
2020-06-13 14:44:39 +0800

    提交代码

---
+++ Main_0613.java
@@ -0,0 +1,18 @@
+package java_0613;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int a = 0;
+        int b = 1;
+        while (b < n) {
+            int tmp = a + b;
+            a = b;
+            b = tmp;
+        }
+        System.out.println(Math.min(n - a, b - n));
+    }
+}
