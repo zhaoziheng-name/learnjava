c3c7fdfd9b92a54b339689c90ac88f937700221f
zhaoziheng-name <527492225@qq.com>
2020-07-30 21:35:27 +0800

    提交代码

---
+++ TestWebsocket.java
@@ -0,0 +1,57 @@
+package test;
+
+import javax.websocket.*;
+import javax.websocket.server.PathParam;
+import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+
+@ServerEndpoint(value="/webSocketTest/{userId}")
+public class TestWebsocket {
+    private int userId;
+
+    // 客户端建立连接时自动调用
+    @OnOpen
+    public void onOpen(@PathParam("userId") String userIdStr, Session session) {
+        System.out.println("建立连接: " + userIdStr);
+        this.userId = Integer.parseInt(userIdStr);
+
+        // 创建一个线程, 通过这个线程, 循环反复的给客户端发送数据
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    String resp = ""+ System.currentTimeMillis();
+                    try {
+                        session.getBasicRemote().sendText(resp);
+                        Thread.sleep(1000);
+                    } catch (IOException | InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+    }
+
+    // 连接被断开的时候自动调用
+    @OnClose
+    public void onClose() {
+        System.out.println("断开连接: " + userId);
+    }
+
+    // 连接异常终止时被自动调用
+    @OnError
+    public void onError(Session session, Throwable error) {
+        System.out.println("连接异常: " + userId);
+        System.out.println(error.getMessage());
+    }
+
+    // 收到消息时被自动调用
+    @OnMessage
+    public void onMessage(String message, Session session) throws IOException {
+        System.out.println("收到消息! message = " + message);
+        // 服务器给客户端返回一个消息
+        // 返回一个当前的时间戳
+        String resp = "" + System.currentTimeMillis();
+        session.getBasicRemote().sendText(resp);
+    }
+}
